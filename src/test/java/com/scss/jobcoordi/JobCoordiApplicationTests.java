553037cea68cd83b735dabd74a0a9672fbb74472
min0962 <wsx8028@gmail.com>
2025-04-04 23:49:22 +0900

    init

---
+++ src/test/java/com/scss/jobcoordi/JobCoordiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scss.jobcoordi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobCoordiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
