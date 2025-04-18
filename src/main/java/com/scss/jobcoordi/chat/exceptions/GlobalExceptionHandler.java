a2f4876e9ce31278b377184f45de1c8b340e995b
min0962 <wsx8028@gmail.com>
2025-04-19 00:08:13 +0900

    [feat] : 나머지 예외 로그 출력 추가

--- src/main/java/com/scss/jobcoordi/chat/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/scss/jobcoordi/chat/exceptions/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.scss.jobcoordi.chat.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -30,6 +32,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> ExceptionException(Exception ex){
+        log.error("예상치 못한 예외 발생", ex);
         return ResponseEntity.status(500).body(new ErrorResponse("생각지도 못한 오류 발생..! 발견 시 알려주기 바람"));
     }
 
